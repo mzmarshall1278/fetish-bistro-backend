5b86f7722556f2a7843e2a3710429bc80c06a89b
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-30 23:47:59 +0100

    created imported package controlller package in module

--- src/package/package.module.ts
+++ src/package/package.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PackageService } from './package.service';
+import { PackageController } from './package.controller';
 
 @Module({
-  providers: [PackageService]
+  providers: [PackageService],
+  controllers: [PackageController]
 })
 export class PackageModule {}
