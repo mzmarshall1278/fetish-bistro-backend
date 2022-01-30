a277f47eabfffb67702c89e081b9a49193eef3a6
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-30 23:55:33 +0100

    created user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PackageModule } from './package/package.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PackageModule],
+  imports: [PackageModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
