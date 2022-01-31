08d2ad71c547afe9a3d551418e3c81ec60fb357a
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-31 23:02:38 +0100

    removed app controller and services import in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PackageModule } from './package/package.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [PackageModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
