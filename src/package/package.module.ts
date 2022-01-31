c7897470f763212dc28224298a53d0890e3e615f
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-31 23:32:02 +0100

    imported mongoose for feature

--- src/package/package.module.ts
+++ src/package/package.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PackageService } from './package.service';
 import { PackageController } from './package.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { packageSchema } from './Package.model';
+import { PackageRepository } from './Package.repository';
 
 @Module({
-  providers: [PackageService],
+  imports: [MongooseModule.forFeature([{name: 'Package', schema: packageSchema}])],
+  providers: [PackageService, PackageRepository],
   controllers: [PackageController]
 })
 export class PackageModule {}
