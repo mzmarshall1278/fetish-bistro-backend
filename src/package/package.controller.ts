75187a41e80e9839ec4e4a5028f7e609314eb4d9
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-31 23:31:13 +0100

    add get all packages in services

--- src/package/package.controller.ts
+++ src/package/package.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Package } from './Package.model';
+import { PackageService } from './package.service';
 
 @Controller('package')
-export class PackageController {}
+export class PackageController {
+    constructor(
+        private packageService: PackageService
+    ){}
+
+    @Get('/')
+    getAllPackages():Promise<Package[]>{
+        return this.packageService.getAllPackages()
+    }
+}
