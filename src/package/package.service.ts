6b28230d5988f7197247ffc92db849f53d23a7b3
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-31 23:32:53 +0100

    added get all packages in services

--- src/package/package.service.ts
+++ src/package/package.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Package } from './Package.model';
+import { PackageRepository } from './Package.repository';
 
 @Injectable()
-export class PackageService {}
+export class PackageService {
+    constructor(
+        private packageRepository: PackageRepository
+    ){}
+
+        async getAllPackages(): Promise<Package[]>{
+            return this.packageRepository.getAllPackages()
+        }
+}
