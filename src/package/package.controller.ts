74ff776ddc97540e7b774eb8d997a93029210c55
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-01 23:35:10 +0100

    added crreatePackage method to controller

--- src/package/package.controller.ts
+++ src/package/package.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { getPackageFilterDto } from './dto/getFilter.dto';
 import { Package } from './Package.model';
 import { PackageService } from './package.service';
+import { CreatePackageDto } from './dto/createPackage.dto';
 
 @Controller('package')
 export class PackageController {
@@ -17,5 +18,10 @@ export class PackageController {
     @Get('/:id')
     getSinglePackage(@Param('id') id: string): Promise<Package>{
         return this.packageService.getSinglePackage(id)
+    }
+
+    @Post('/')
+    addPackage(@Body() createPackageDto: CreatePackageDto): Promise<Package>{
+        return this.packageService.createPackage(createPackageDto);
     }
 }
