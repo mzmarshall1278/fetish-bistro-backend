6d4ae9dc7204823a82978515912b2ad1081f5a6f
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-01 23:35:35 +0100

    added crreatePackage method to service

--- src/package/package.service.ts
+++ src/package/package.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { getPackageFilterDto } from './dto/getFilter.dto';
 import { Package } from './Package.model';
 import { PackageRepository } from './Package.repository';
+import { CreatePackageDto } from './dto/createPackage.dto';
 
 @Injectable()
 export class PackageService {
@@ -15,5 +16,9 @@ export class PackageService {
 
         async getSinglePackage(id: string): Promise<Package> {
             return this.packageRepository.getSinglePackage(id)
+        }
+
+        async createPackage(createPackageDto: CreatePackageDto): Promise<Package>{
+            return this.packageRepository.createPackage(createPackageDto);
         }
 }
