bdf4a4116c9dbe71c026e01223b447cbd889b28e
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-01 23:35:24 +0100

    created crreatePackage method in repository

--- src/package/Package.repository.ts
+++ src/package/Package.repository.ts
@@ -3,6 +3,7 @@ import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { getPackageFilterDto } from "./dto/getFilter.dto";
 import { Package } from "./Package.model";
+import { CreatePackageDto } from './dto/createPackage.dto';
 
 @Injectable()
 export class PackageRepository {
@@ -19,6 +20,14 @@ export class PackageRepository {
         const found = await this.Package.findById(id);
         if(!found) throw new NotFoundException('The package could not be found');
         return found;
+    }
+
+    async createPackage(createPackageDto: CreatePackageDto): Promise<Package>{
+        const {name, price, description, available, imageUrl, quantity} = createPackageDto;
+        const savedPackage = await new this.Package({
+            name, price, description, available, imageUrl, quantity
+        }).save();
+        return savedPackage;
     }
 
 }
