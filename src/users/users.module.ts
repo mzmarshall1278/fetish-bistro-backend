c471ef853fd6f45b2b8bbb1ac91bdfb7a2bc818e
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-02 23:59:12 +0100

    injected user repository

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,12 @@
+import { UserRepository } from './User.repository';
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  providers: [UsersService],
+  imports: [MongooseModule.forFeature([])],
+  providers: [UsersService, UserRepository],
   controllers: [UsersController]
 })
 export class UsersModule {}
