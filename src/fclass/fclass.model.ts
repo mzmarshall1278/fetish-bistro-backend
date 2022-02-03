b0dc25967fe8ba24909f4cd2aa1cf31481119c70
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-03 23:44:48 +0100

    created fclass model

---
+++ src/fclass/fclass.model.ts
@@ -0,0 +1,31 @@
+import * as mongoose from 'mongoose'
+
+export const fclassSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+    fee: {type: Number, required: true},
+    duration: {type: Number, required: true},
+    startDate: {type: String, required: true},
+    endDate: {type: String, required: true},
+    status: {type: String, enum: ['Open', 'Started', 'Finished'], required: true},
+    registeredUsers: {type: Array, required: true},
+    discount: {type: String, required: true},
+})
+
+export enum StatusType {
+    open = 'Open',
+    started = 'Started',
+    finished = 'Finished',
+}
+
+export interface FClass {
+    name: string;
+    description: string;
+    fee: number;
+    duration: number;
+    startDate: string;
+    endDate: string;
+    status: StatusType;
+    registeredUsers: []
+    discount: number
+}
