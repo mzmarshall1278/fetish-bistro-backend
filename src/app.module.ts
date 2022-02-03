c4b25661c45a0a7691a28fdc27c7e378e7df99da
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-03 23:24:11 +0100

    removed user model imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PackageModule } from './package/package.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/fetish'),
-    PackageModule,
-    UsersModule],
+    PackageModule],
 })
 export class AppModule {}
