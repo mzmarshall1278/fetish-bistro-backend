dd344415b9428ea2b52fba09b008c9605ab838ca
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-03 23:52:06 +0100

    imported fclass repository and registered the schema

--- src/fclass/fclass.module.ts
+++ src/fclass/fclass.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { FclassService } from './fclass.service';
 import { FclassController } from './fclass.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FclassSchema } from './fclass.model';
+import { FClassRepository } from './fclass.repository';
 
 @Module({
-  providers: [FclassService],
+  imports: [
+    MongooseModule.forFeature([{name: 'FClass', schema: FclassSchema}])
+  ],
+  providers: [FclassService, FClassRepository],
   controllers: [FclassController]
 })
 export class FclassModule {}
