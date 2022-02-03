cd5b5c9a8ddc1cc93cdcc7b17e08ce113ffd45ec
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-03 23:26:17 +0100

    add fclass to app.ja

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PackageModule } from './package/package.module';
+import { FclassModule } from './fclass/fclass.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/fetish'),
-    PackageModule],
+    PackageModule,
+    FclassModule],
 })
 export class AppModule {}
