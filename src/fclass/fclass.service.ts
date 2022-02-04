fc6619a49a6f0bf27e7f7fd572579e6268523640
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-04 23:32:10 +0100

    add registerForAClass service

--- src/fclass/fclass.service.ts
+++ src/fclass/fclass.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { FClass } from './fclass.model';
 import { FClassRepository } from './fclass.repository';
+import { RegisterClassDto } from './dto/registerClass.dto';
 
 @Injectable()
 export class FclassService {
@@ -10,5 +11,9 @@ export class FclassService {
 
     async getAllClasses(): Promise<FClass[]> {
         return this.fclsaaRepository.getAllClasses()
+    }
+
+    async registerForClass (registerDto: RegisterClassDto){
+        return this.fclsaaRepository.registerForClass(registerDto);
     }
 }
