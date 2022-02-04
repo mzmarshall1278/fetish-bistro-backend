db08ccdda4870af6d369959b12387dd854e001e5
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-04 23:32:52 +0100

    Created registerForAClass method in repository

--- src/fclass/fclass.repository.ts
+++ src/fclass/fclass.repository.ts
@@ -1,5 +1,6 @@
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
+import { RegisterClassDto } from "./dto/registerClass.dto";
 import { FClass } from "./fclass.model";
 
 export class FClassRepository {
@@ -10,5 +11,10 @@ export class FClassRepository {
 
     async getAllClasses(): Promise<FClass[]> {
         return this.FClass.aggregate([]);
+    }
+
+    async registerForClass(registerDto: RegisterClassDto){
+        const {id, user, date} = registerDto;
+        return this.FClass.updateOne({id}, {$push: {registeredUsers: {user, date}}})
     }
 }
