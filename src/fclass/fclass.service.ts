80aa592e3951212a8f5e6f4de5b319890696396f
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-04 23:36:31 +0100

    add return typr

--- src/fclass/fclass.service.ts
+++ src/fclass/fclass.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { FClass } from './fclass.model';
 import { FClassRepository } from './fclass.repository';
 import { RegisterClassDto } from './dto/registerClass.dto';
+import { UpdateWriteOpResult } from 'mongoose';
 
 @Injectable()
 export class FclassService {
@@ -13,7 +14,7 @@ export class FclassService {
         return this.fclsaaRepository.getAllClasses()
     }
 
-    async registerForClass (registerDto: RegisterClassDto){
+    async registerForClass (registerDto: RegisterClassDto): Promise<UpdateWriteOpResult>{
         return this.fclsaaRepository.registerForClass(registerDto);
     }
 }
