bf29f3965d6acd1fccdd83ab2819f2f00b6c927a
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-04 23:16:53 +0100

    created getAllClasses service

--- src/fclass/fclass.service.ts
+++ src/fclass/fclass.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { FClassRepository } from './fclass.repository';
 
 @Injectable()
-export class FclassService {}
+export class FclassService {
+    constructor(
+        private fclsaaRepository: FClassRepository
+    ){}
+
+    async getAllClasses(){
+        return this.fclsaaRepository.getAllClasses()
+    }
+}
