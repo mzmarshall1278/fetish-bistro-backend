8ca365e999ddcaf324c0bbcd92c34d683deff566
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-05 23:48:32 +0100

    created user model

---
+++ src/auth/user.model.ts
@@ -0,0 +1,28 @@
+import * as mongoose from 'mongoose'
+
+export enum UserType {
+    admin = 'Admin',
+    regular = 'Regular',
+    developer = 'Developer'
+}
+
+export const UserSchema = new mongoose.Schema({
+    email: {type: String, required: true},
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    salt: {type: String, required: true},
+    userType: {type: String, enum: UserType, required: true},
+    orders: {type: Array, required: true},
+    classes: {type: String, required: true},
+})
+
+export interface User {
+    email: string;
+    username: string;
+    password: string;
+    salt: string;
+    UserType: UserType;
+    orders: [];
+    classes: [];
+}
+
