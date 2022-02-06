73b8f0b17ace3a4da1d5b9a3aa6df0fb49c069a8
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-06 22:56:38 +0100

    inject user schema and auth repository

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,15 @@
+import { AuthRepository } from './auth.repository';
+import { UserSchema } from './user.model';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{name: 'User', schema: UserSchema}])
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, AuthRepository]
 })
 export class AuthModule {}
