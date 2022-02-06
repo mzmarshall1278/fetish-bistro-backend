a3c5a674f69132b8ab9e81294ff7da6c6c870ec7
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-06 23:16:03 +0100

    created order model

---
+++ src/order/order.model.ts
@@ -0,0 +1,30 @@
+import * as mongoose from 'mongoose'
+import { User } from 'src/auth/user.model'
+import { Package } from 'src/package/Package.model'
+
+export enum OrderStatus {
+    new = 'New',
+    pending = 'Pending',
+    paid = 'Paid',
+    enroute = 'Enroute',
+    delivered = 'Delivered'
+}
+
+export const orderSchema = new mongoose.Schema({
+    user: {},
+    package: {},
+    quantity: {},
+    price: {},
+    date: {},
+    status: {}
+})
+
+export class Order {
+    user: User
+    package: Package;
+    quantity: number
+    price: number;
+    date: string;
+    status: OrderStatus
+}
+
