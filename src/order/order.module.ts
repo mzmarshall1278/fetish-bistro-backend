9559469090c8aa455857caf48927975c9fc49185
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-06 23:20:51 +0100

    injected order repository and order schema

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { OrderSchema } from './order.model';
+import { OrderRepository } from './order.repository';
 
 @Module({
-  providers: [OrderService],
+  imports: [
+    MongooseModule.forFeature([{name: 'Order', schema: OrderSchema}])
+  ],
+  providers: [OrderService, OrderRepository],
   controllers: [OrderController]
 })
 export class OrderModule {}
