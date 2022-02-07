cb09011b1b4e51b33e1414c41dc2de4cfb925e9a
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-07 23:28:59 +0100

    add makeOrder method in repository

--- src/order/order.repository.ts
+++ src/order/order.repository.ts
@@ -2,10 +2,14 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Order } from "./order.model";
+import { CreateOrderDto } from './dto/createOrder.dto';
 
 @Injectable()
 export class OrderRepository {
     constructor(@InjectModel('Order') private readonly Order: Model<Order>){}
 
-    
+    async makeOrder (createOrderDto: CreateOrderDto): Promise<Order>{
+        const order = new this.Order(createOrderDto).save();
+        return order;
+    }
 }
