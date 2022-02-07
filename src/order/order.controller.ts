2ba9eecc52d255060670e6a6fedac5fe9c0c1066
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-07 23:28:14 +0100

    add createOrder method in controller

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
+import { OrderService } from './order.service';
+import { CreateOrderDto } from './dto/createOrder.dto';
+import { Order } from './order.model';
 
 @Controller('order')
-export class OrderController {}
+export class OrderController {
+    constructor(private orderService: OrderService){}
+
+    @Get('/')
+    makeOrder(@Body() createOrderDto: CreateOrderDto): Promise<Order> {
+        return this.orderService.makeOrder(createOrderDto);
+    }
+}
