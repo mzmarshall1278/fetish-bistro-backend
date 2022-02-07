6345c323266576059c518bb30ef2e02f154840f2
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-07 23:29:17 +0100

    add makeOrder method in service

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { OrderRepository } from './order.repository';
+import { CreateOrderDto } from './dto/createOrder.dto';
+import { Order } from './order.model';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+    constructor(private orderRepository: OrderRepository){}
+
+    async makeOrder (createOrderDto: CreateOrderDto): Promise<Order>{
+        return this.orderRepository.makeOrder(createOrderDto);
+    }
+}
