f7843fc8e0b131e078aa5037b1a6ab6738d7ba34
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-08 12:57:56 +0100

    add two methods to controller for creating and confirming registration

--- src/registration/registration.controller.ts
+++ src/registration/registration.controller.ts
@@ -1,8 +1,17 @@
 import { RegistrationService } from './registration.service';
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { RegisterClassDto } from './dto/registerClass.dto';
+import { Registration } from './registration.model';
 
 @Controller('registration')
 export class RegistrationController {
     constructor(private registrationService: RegistrationService){}
-    
+    @Post('/')
+    registerForClass(@Body() registerClassDto: RegisterClassDto): Promise<Registration> {
+        return this.registrationService.registerForClass(registerClassDto);
+    }
+
+    confirmRegistration(@Param() registrationId: string): Promise<Registration> {
+        return this.registrationService.confirmRegistration(registrationId);
+    }
 }
