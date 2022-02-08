60ba9162357169784fa9158a337e47d0f92c0d58
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-08 12:21:51 +0100

    injected registration repository and schema

--- src/registration/registration.module.ts
+++ src/registration/registration.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { RegistrationController } from './registration.controller';
 import { RegistrationService } from './registration.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RegistrationSchema } from './registration.model';
+import { RegistrationRepository } from './registration.repository';
+
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{name: 'Registration', schema: RegistrationSchema}])
+  ],
   controllers: [RegistrationController],
-  providers: [RegistrationService]
+  providers: [RegistrationService, RegistrationRepository]
 })
 export class RegistrationModule {}
