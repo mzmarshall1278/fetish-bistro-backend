863a3b805b63874e23311d6a2afe589b32ac8aa9
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-08 12:58:35 +0100

    add two methods in service  for creating and confirming registration

--- src/registration/registration.service.ts
+++ src/registration/registration.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { RegistrationRepository } from './registration.repository';
+import { RegisterClassDto } from './dto/registerClass.dto';
+import { Registration } from './registration.model';
 
 @Injectable()
-export class RegistrationService {}
+export class RegistrationService {
+    constructor (private registrationRepository: RegistrationRepository){}
+
+    async registerForClass(registerClass: RegisterClassDto): Promise<Registration> {
+        return this.registrationRepository.registerForClass(registerClass);
+    }
+
+    async confirmRegistration(registrationId: string): Promise<Registration>{
+        return this.registrationRepository.confirmRegistration(registrationId);
+    }
+}
