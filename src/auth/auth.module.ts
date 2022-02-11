3f391442f6f26fb5df274cdfa474a5f6b2476a5e
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-11 23:50:28 +0100

    injected jwt startegy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,23 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{name: 'User', schema: UserSchema}])
+    MongooseModule.forFeature([{name: 'User', schema: UserSchema}]),
+    PassportModule.register({defaultStrategy: 'jwt'}),
+    JwtModule.register({
+      secret: 'aN0th3R5pec1alc0d3',
+      signOptions: {
+        expiresIn: 3600 * 24
+      }
+    })
   ],
   controllers: [AuthController],
-  providers: [AuthService, AuthRepository]
+  providers: [AuthService, AuthRepository, JwtStrategy],
+  exports: [JwtStrategy,PassportModule]
 })
 export class AuthModule {}
