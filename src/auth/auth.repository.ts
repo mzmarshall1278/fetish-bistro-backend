e9e7488c15cac5a1058ce5052c883220e4be7497
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-12 23:49:47 +0100

    created signup dto

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { User } from "./user.model";
+import { SignupDto } from './dto/signup.dto';
 
 
 @Injectable()
@@ -10,4 +11,8 @@ export class AuthRepository {
         @InjectModel('User')
         private readonly User: Model<User>
     ) {}
+
+    async signup (credentials: SignupDto) {
+        const {username, password, email, phoneNumber} = credentials;
+    }
 }
