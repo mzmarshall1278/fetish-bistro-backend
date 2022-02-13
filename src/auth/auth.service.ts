f9b34221e36a037b525e8c94783381f506137165
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-13 23:53:17 +0100

    addae signup in service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { AuthRepository } from './auth.repository';
+import { SignupDto } from './dto/signup.dto';
 
 @Injectable()
 export class AuthService {
 
     constructor(private authRepository: AuthRepository){}
 
+    signup (credentials: SignupDto): Promise<string> {
+        return this.authRepository.signup(credentials)
+    }
     
 }
