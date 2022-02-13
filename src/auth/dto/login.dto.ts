04391992baa2bed496757684ce6b75f9c2942cb6
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-13 23:55:18 +0100

    craeted Login dto

---
+++ src/auth/dto/login.dto.ts
@@ -0,0 +1,14 @@
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class LoginDto {
+    @IsOptional()
+    @IsString()
+    username: string;
+
+    @IsOptional()
+    @IsString()
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    password: string}
