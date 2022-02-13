59fd6c72ff6b69c744ee0e4a22f00d378ecedbc4
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-13 23:53:00 +0100

    created signup method in repository

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -1,8 +1,9 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { User } from "./user.model";
 import { SignupDto } from './dto/signup.dto';
+import bcrypt from 'bcrypt';
 
 
 @Injectable()
@@ -12,7 +13,25 @@ export class AuthRepository {
         private readonly User: Model<User>
     ) {}
 
-    async signup (credentials: SignupDto) {
+    async signup (credentials: SignupDto): Promise<string> {
         const {username, password, email, phoneNumber} = credentials;
+
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await this.hashPassword(password ,salt);
+
+        try {
+            const found = await this.User.findOne({ username });
+           if(found) {
+            throw new ConflictException('Username already taken');
+           }
+            await new this.User({username, password: hashedPassword, userType: 'ADMIN', salt, orders: [], classes: [], email, phoneNumber}).save();
+            return 'Account Created successfully'
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    private async hashPassword (password: string, salt: string):Promise<string> {
+        return bcrypt.hash(password, salt);
     }
 }
