4d1e75c4acc03e1f7788e530446d2965854ae8cb
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-14 22:48:08 +0100

    add getLoggedInUser in controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { SignupDto } from './dto/signup.dto';
 import { LoginDto } from './dto/login.dto';
 import { AuthService } from './auth.service';
+import { GetUser } from './getUser.decorator';
+import { User } from './user.model';
+import { JwtPayload } from './jwt.payload';
 
 @Controller('auth')
 export class AuthController {
@@ -17,4 +20,10 @@ export class AuthController {
     login(@Body() credentials: LoginDto): Promise<{accessToken: string}> {
         return this.authService.login(credentials);
     }
+
+    @Get('/user')
+    getLoggedInUser(@GetUser() user: User): Promise<JwtPayload> {
+        return this.authService.getLoggedInUser(user);
+    }
+
 }
