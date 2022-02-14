70f90f800e415a34130ab2d4237f2cc3f424e440
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-14 22:33:58 +0100

    add login to services

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { AuthRepository } from './auth.repository';
 import { SignupDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
 
     constructor(private authRepository: AuthRepository){}
 
-    signup (credentials: SignupDto): Promise<string> {
+    async signup (credentials: SignupDto): Promise<string> {
         return this.authRepository.signup(credentials)
+    }
+
+    async login(credentials: LoginDto): Promise<{accessToken: string}> {
+        return this.authRepository.login(credentials);
     }
     
 }
