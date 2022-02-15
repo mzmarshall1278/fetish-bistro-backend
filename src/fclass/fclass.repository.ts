1475f5f2e27f8bf0b5ab1ed97eb85ab97faa1497
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-16 00:00:15 +0100

    strict access to some methods for admin only

--- src/fclass/fclass.repository.ts
+++ src/fclass/fclass.repository.ts
@@ -1,10 +1,11 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { CreateClassDto } from "./dto/createClass.dto";
 import { FClass } from "./fclass.model";
 import { RegistrationService } from '../registration/registration.service';
 import { Registration } from "src/registration/registration.model";
+import { User } from "src/auth/user.model";
 
 @Injectable()
 export class FClassRepository {
@@ -18,12 +19,14 @@ export class FClassRepository {
         return this.FClass.aggregate([]);
     }
 
-    async createClass(createClassDto: CreateClassDto): Promise<FClass>{
+    async createClass(createClassDto: CreateClassDto, user: User): Promise<FClass>{
+        if(user.UserType != 'Admin') throw new UnauthorizedException('You are not Authorized');
         const fClass = await new this.FClass(createClassDto).save()
         return fClass;
     }
 
-    async closeFclassRegistration (id: string): Promise<FClass> {
+    async closeFclassRegistration (id: string, user: User): Promise<FClass> {
+        if(user.UserType != 'Admin') throw new UnauthorizedException('You are not Authorized');
         const fclass = await this.FClass.findOneAndUpdate({_id: id},{$set: {openForRegistration: false}});
         return fclass
     }
