0d9dfa42c475648d52658bbdb3552b8e98276773
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-15 23:47:26 +0100

    add authGuards in package controller

--- src/package/package.controller.ts
+++ src/package/package.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { getPackageFilterDto } from './dto/getFilter.dto';
 import { Package } from './Package.model';
 import { PackageService } from './package.service';
 import { CreatePackageDto } from './dto/createPackage.dto';
 import { AddCommentDto } from './dto/addComment.dto';
 import { UpdatePackageDto } from './dto/updatePackage.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('package')
 export class PackageController {
@@ -23,16 +24,19 @@ export class PackageController {
     }
 
     @Post('/')
+    @UseGuards(AuthGuard())
     addPackage(@Body() createPackageDto: CreatePackageDto): Promise<Package>{
         return this.packageService.createPackage(createPackageDto);
     }
 
     @Post('/:id')
+    @UseGuards(AuthGuard())
     addComment(@Body() addCommentDto: AddCommentDto):Promise<Package> {
         return this.packageService.addComment(addCommentDto);
     }
 
     @Put('/:id')
+    @UseGuards(AuthGuard())
     updatePackage(@Body() updatePackageDto: UpdatePackageDto): Promise<Package>{
         return this.packageService.updatePackage(updatePackageDto);
     }
