c0d8d9ca99d258f04e5ad77bd2a1a0890bc87358
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-16 23:36:57 +0100

    add user object in order service

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -2,20 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { OrderRepository } from './order.repository';
 import { CreateOrderDto } from './dto/createOrder.dto';
 import { Order, OrderStatus } from './order.model';
+import { User } from 'src/auth/user.model';
 
 @Injectable()
 export class OrderService {
     constructor(private orderRepository: OrderRepository){}
 
-    async makeOrder (createOrderDto: CreateOrderDto): Promise<Order>{
-        return this.orderRepository.makeOrder(createOrderDto);
+    async makeOrder (createOrderDto: CreateOrderDto, user: User): Promise<Order>{
+        return this.orderRepository.makeOrder(createOrderDto, user);
     }
 
-    async UpdateOrderStatus(id: string, status: OrderStatus): Promise<Order> {
-        return this.orderRepository.UpdateOrderStatus(id, status);
+    async UpdateOrderStatus(id: string, status: OrderStatus, user: User): Promise<Order> {
+        return this.orderRepository.UpdateOrderStatus(id, status, user);
     }
 
-    async getUsersOrders (userId: string): Promise<Order[]> {
-        return this.orderRepository.getUsersOrders(userId)
+    async getUsersOrders (userId: string, user: User): Promise<Order[]> {
+        return this.orderRepository.getUsersOrders(userId, user);
     }
 }
