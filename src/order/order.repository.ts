c989a9a7c4e39fa67c7848c102901f54c39be48d
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-16 23:36:49 +0100

    add user object in order repository

--- src/order/order.repository.ts
+++ src/order/order.repository.ts
@@ -1,23 +1,26 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Order, OrderStatus } from './order.model';
 import { CreateOrderDto } from './dto/createOrder.dto';
+import { User } from "src/auth/user.model";
 
 @Injectable()
 export class OrderRepository {
     constructor(@InjectModel('Order') private readonly Order: Model<Order>){}
 
-    async makeOrder (createOrderDto: CreateOrderDto): Promise<Order>{
-        const order = new this.Order({...createOrderDto, status: 'New'}).save();
+    async makeOrder (createOrderDto: CreateOrderDto, user: User): Promise<Order>{
+        const order = new this.Order({...createOrderDto, status: 'New', user: user.id}).save();
         return order;
     }
 
-    async UpdateOrderStatus(id: string, status: OrderStatus): Promise<Order> {
-        return this.Order.findByIdAndUpdate({id}, {$set:{status}})
+    async UpdateOrderStatus(id: string, status: OrderStatus, user: User): Promise<Order> {
+        if(user.UserType !== 'Admin') throw new UnauthorizedException('Not Authorized!!!');
+        return this.Order.findByIdAndUpdate({id, user: user.id}, {$set:{status}});
     }
 
-    async getUsersOrders (userId: string): Promise<Order[]> {
+    async getUsersOrders (userId: string, user: User): Promise<Order[]> {
+        if(user.id !== userId || user.UserType !== 'Admin') throw new UnauthorizedException('Not Authorized');
         return this.Order.aggregate([
             {$match: {user: userId}}
         ])
