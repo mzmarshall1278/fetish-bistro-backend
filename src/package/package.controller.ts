70e9284e587fed31cc594742b5a43a8130fad6fe
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-16 23:46:55 +0100

    add user object in controller

--- src/package/package.controller.ts
+++ src/package/package.controller.ts
@@ -6,6 +6,8 @@ import { CreatePackageDto } from './dto/createPackage.dto';
 import { AddCommentDto } from './dto/addComment.dto';
 import { UpdatePackageDto } from './dto/updatePackage.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/auth/getUser.decorator';
+import { User } from 'src/auth/user.model';
 
 @Controller('package')
 export class PackageController {
@@ -25,19 +27,19 @@ export class PackageController {
 
     @Post('/')
     @UseGuards(AuthGuard())
-    addPackage(@Body() createPackageDto: CreatePackageDto): Promise<Package>{
-        return this.packageService.createPackage(createPackageDto);
+    addPackage(@Body() createPackageDto: CreatePackageDto, @GetUser() user: User): Promise<Package>{
+        return this.packageService.createPackage(createPackageDto, user);
     }
 
     @Post('/:id')
     @UseGuards(AuthGuard())
-    addComment(@Body() addCommentDto: AddCommentDto):Promise<Package> {
-        return this.packageService.addComment(addCommentDto);
+    addComment(@Body() addCommentDto: AddCommentDto, @GetUser() user: User):Promise<Package> {
+        return this.packageService.addComment(addCommentDto, user);
     }
 
     @Put('/:id')
     @UseGuards(AuthGuard())
-    updatePackage(@Body() updatePackageDto: UpdatePackageDto): Promise<Package>{
-        return this.packageService.updatePackage(updatePackageDto);
+    updatePackage(@Body() updatePackageDto: UpdatePackageDto, @GetUser() user: User): Promise<Package>{
+        return this.packageService.updatePackage(updatePackageDto, user);
     }
 }
